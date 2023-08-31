536ea604ef71fdd1e32c924b3e00c634dc8b96af
chandresh kumar <kchandresh726@gmail.com>
2023-08-31 16:32:30 +0530

    bug fix

--- app/sections/about/index.jsx
+++ app/sections/about/index.jsx
@@ -25,18 +25,18 @@ export function AboutSection() {
 						}}
 					>
 						<p>
-						I'm Chandresh Kumar, a dedicated software engineer with a passion for coding and a strong foundation in programming languages 
-						such as Java, Python, and JavaScript. My journey into the tech world began during my undergraduate studies in Computer Science at 
-						Keshav Memorial Institute of Technology, where I excelled academically with a GPA of 8.0.
+							I&apos;m Chandresh Kumar, a dedicated software engineer with a passion for coding and a strong foundation in programming languages 
+							such as Java, Python, and JavaScript. My journey into the tech world began during my undergraduate studies in Computer Science at 
+							Keshav Memorial Institute of Technology, where I excelled academically with a GPA of 8.0.
 						</p>
 						<p className="my-3.5">
-							Beyond the screen, I'm an avid problem solver and a competitive coder. When I'm not crafting digital solutions, you can find me delving into 
-							outdoor activities and video games or unwinding with a good movie and some music.
+							Beyond the screen, I&apos;m an avid problem solver and a competitive coder. When I&apos;m not crafting digital solutions, you can find me 
+							delving into outdoor activities and video games or unwinding with a good movie and some music.
 						</p>
 						<p className="my-3.5">
 							With hands-on experience in web development technologies like ReactJS and Flask, and a knack for building seamless CI/CD pipelines 
-							using Jenkins, I'm always ready to take on new challenges in the dynamic world of software engineering. Whether it's crafting 
-							user-friendly web applications or engaging in some friendly coding competition, I'm always up for the next exciting venture.
+							using Jenkins, I&apos;m always ready to take on new challenges in the dynamic world of software engineering. Whether it&apos;s crafting 
+							user-friendly web applications or engaging in some friendly coding competition, I&apos;m always up for the next exciting venture.
 						</p>
 					</div>
 				</div>
