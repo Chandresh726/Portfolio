d90f1ae37566a55bbce7ba163eca3fe7598e5e46
chandresh kumar <kchandresh726@gmail.com>
2024-10-05 14:12:27 +0530

    added animation to lottie

--- app/sections/introduction/DisplayLottie.jsx
+++ app/sections/introduction/DisplayLottie.jsx
@@ -1,18 +1,29 @@
-import React from "react";
+import { useRef } from "react";
+import { motion, useInView } from "framer-motion";
 import Lottie from "react-lottie";
 
 const GreetingLottie = ({ animationPath }) => {
-	const defaultOptions = {
-		loop: true,
-		autoplay: true,
-		path: animationPath,
-	};
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
 
-	return (
-		<div onClick={() => null}>
-			<Lottie options={defaultOptions} />
-		</div>
-	);
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    path: animationPath,
+  };
+
+  return (
+    <motion.div
+      ref={ref}
+      style={{
+        transform: isInView ? "none" : "translateX(-200px)",
+        opacity: isInView ? 1 : 0,
+        transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.2s",
+      }}
+    >
+      <Lottie options={defaultOptions} />
+    </motion.div>
+  );
 };
 
 export default GreetingLottie;
