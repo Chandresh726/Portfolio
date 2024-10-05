0fc18b8ac41944329b89f0d7d7cbc04363751301
chandresh kumar <kchandresh726@gmail.com>
2024-10-05 14:02:29 +0530

    added new project

--- constants/projects.js
+++ constants/projects.js
@@ -1,34 +1,42 @@
 export const PROJECTS = [
     {
-        title : "Samraksha",
-        description : "MERN stack application with QR Code implementation for the management of student's movements in and out of the hostel and managing attendance.",
-        images : ["/images/samraksha/1.png","/images/samraksha/2.png","/images/samraksha/3.png","/images/samraksha/4.png"],
-        liveUrl : "https://samraksha-test.onrender.com/",
-        repoUrl : "https://github.com/Chandresh726/samraksha",
-        stack : ["ReactJS","MongoDB","ExpressJS","NodeJS"]
+        title: "DeTube",
+        description: "A Decentralized Video Streaming Platform Powered by Solana Blockchain.",
+        images: ["/images/detube/1.png", "/images/detube/2.png", "/images/detube/3.png", "/images/detube/4.png", "/images/detube/5.png"],
+        liveUrl: "https://detube-slope.vercel.app/",
+        repoUrl: "https://github.com/Chandresh726/DeTube",
+        stack: ["NextJS", "Tailwind CSS", "AWS", "Solana"]
     },
     {
-        title : "Sun Minerals",
-        description : "Single Page React Application",
-        images : ["/images/sun_minerals/1.png","/images/sun_minerals/2.png","/images/sun_minerals/3.png"],
-        liveUrl : "https://www.sunminerals.in/",
-        repoUrl : "https://github.com/Chandresh726/CoalSite",
-        stack : ["ReactJS"]
+        title: "Portfolio",
+        description: "Developer Portfolio Page build using Next.js",
+        images: ["/images/portfolio/1.png", "/images/portfolio/2.png", "/images/portfolio/3.png"],
+        liveUrl: "https://portfolio-chandresh.vercel.app/",
+        repoUrl: "https://github.com/Chandresh726/Portfolio",
+        stack: ["NextJS", "Tailwind CSS"]
     },
     {
-        title : "Portfolio",
-        description : "Developer Portfolio Page build using Next.js",
-        images : ["/images/portfolio/1.png","/images/sun_minerals/2.png"],
-        liveUrl : "https://portfolio-chandresh.vercel.app/",
-        repoUrl : "https://github.com/Chandresh726/Portfolio",
-        stack : ["NextJS","Tailwind CSS"]
+        title: "Samraksha",
+        description: "MERN stack application with QR Code implementation for the management of student's movements in and out of the hostel and managing attendance.",
+        images: ["/images/samraksha/1.png", "/images/samraksha/2.png", "/images/samraksha/3.png", "/images/samraksha/4.png"],
+        liveUrl: "https://samraksha-test.onrender.com/",
+        repoUrl: "https://github.com/Chandresh726/samraksha",
+        stack: ["ReactJS", "MongoDB", "ExpressJS", "NodeJS"]
     },
     {
-        title : "KISAN",
-        description : "Web application based on Flask, MongoDB, and ReactJS for farmers, private sectors, and government to help farmers with their ...",
-        images : [],
-        liveUrl : null,
-        repoUrl : "https://github.com/Chandresh726/Lexicon-Agriculture_Portal",
-        stack : ["ReactJS","MongoDB","Flask"]
+        title: "Sun Minerals",
+        description: "Single Page React Application",
+        images: ["/images/sun_minerals/1.png", "/images/sun_minerals/2.png", "/images/sun_minerals/3.png"],
+        liveUrl: "https://coal-site.vercel.app/",
+        repoUrl: "https://github.com/Chandresh726/CoalSite",
+        stack: ["ReactJS"]
+    },
+    {
+        title: "KISAN",
+        description: "Web application based on Flask, MongoDB, and ReactJS for farmers, private sectors, and government to help farmers with their grievances, provide farmers with information about in-demand crops and view statistics of crops prices in different states and post blogs.",
+        images: [],
+        liveUrl: null,
+        repoUrl: "https://github.com/Chandresh726/Lexicon-Agriculture_Portal",
+        stack: ["ReactJS", "MongoDB", "Flask"]
     }
 ];
